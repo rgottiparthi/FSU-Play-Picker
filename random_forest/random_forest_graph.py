036f99d47082525385992f4ce4d5f98105adc1c6
rgottiparthi <rohith.gottiparthi@gmail.com>
2023-12-08 18:34:58 -0500

    changed field goal to two words

--- random_forest/random_forest_graph.py
+++ random_forest/random_forest_graph.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 # Load the results CSV file
 results_df = pd.read_csv('predictions_results.csv')
 
+# Extract the first two words from each label
+results_df['Short_Label'] = results_df['Actual_Best_Play'].apply(lambda x: ' '.join(x.split()[:2]))
+
 # Calculate the likelihood of correct predictions for each play type
-correct_prediction_likelihood = results_df.groupby('Actual_Best_Play')['Correct_Prediction'].mean()
+correct_prediction_likelihood = results_df.groupby('Short_Label')['Correct_Prediction'].mean()
 
 # Plot a bar graph with wider bars and rotated x-axis labels
 bar_width = 0.7
@@ -14,9 +17,9 @@ plt.bar(correct_prediction_likelihood.index, correct_prediction_likelihood * 100
 # Rotate x-axis labels and adjust their alignment
 plt.xticks(rotation='vertical', ha='center')
 
-plt.xlabel('Actual_Best_Play')
+plt.xlabel('Short_Label')
 plt.ylabel('Correct Prediction Likelihood (%)')
-plt.title('Correct Prediction Likelihood for Each Play Type')
+plt.title('Random Forest Likelihood')
 
 # Save the plot as an image file (e.g., PNG)
 plt.tight_layout()
